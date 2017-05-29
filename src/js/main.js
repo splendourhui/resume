58a9e2305555b4a746feb994779e73dc59638584
splendourhui <splendourhui@foxmail.com>
2017-05-29 23:37:55 +0800

    [bugfix] 技能水平条目错误的问题

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,14 @@
-jQuery(document).ready(function ($) {
-  $('.level-bar-inner').css('width', '0');
-  $(window).on('load', function () {
-    $('.level-bar-inner').each(function () {
-      var itemWidth = $(this).data('level');
-      $(this).animate({
-        width: itemWidth
-      }, 800);
-    });
-  });
-});
+// jQuery(document).ready(function ($) {
+//   $('.level-bar-inner').css('width', '0');
+//   $(window).on('load', function () {
+//     $('.level-bar-inner').each(function () {
+//       var itemWidth = $(this).data('level');
+//       $(this).animate({
+//         width: itemWidth
+//       }, 800);
+//     });
+//   });
+// });
 
 function downloadPDF() {
   var pdf = new jsPDF('p', 'pt', 'letter');
